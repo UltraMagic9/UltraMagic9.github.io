989e0630251a0298c7c1bddf794e89693b59ef63
ultraelement23 <yukannakuma@gmail.com>
2017-02-11 16:45:26 +0000

    I updated this file because it was not cooperating with me.

--- calculator.js
+++ calculator.js
@@ -1,32 +1,57 @@
 var number = prompt("First number?");
 var operator = prompt("Which operation? (Add, Subtract, Multiply, Divide)");
 var number2 = prompt("Second number?");
-function divide(num1, num2){
-  window.alert(num1 / num2);
-}
-if (operator == "Divide")
-{
-  this.divide(number, number2);
-}
 
-function multiply(num1, num2){
-  window.alert(num1 * num2);
-}
-if(operator == "Multiply"){
-  this.multiply(number, number2)
-}
+myCalc();
 
-function add(num1, num2){
- var answer=parseInt(num1) + parseInt(num2);
-  window.alert("The answer is " +answer);
-}
-if(operator == "Add"){
-  this.add(number, number2)
-}
+function myCalc() {
+  var noodle = ("yum");
 
-function subtract(num1, num2){
-  window.alert(num1 - num2);
-}
-if(operator == "Subtract"){
-  this.subtract(number, number2)
+  init();
+
+  function init() {
+
+    operator = operator.toLowerCase();
+
+    switch (operator) {
+      case "divide":
+      case "/":
+        divide(number, number2);
+        break;
+      case "multiply":
+      case "*":
+        multiply(number, number2);
+        break;
+      case "add":
+      case "+":
+        add(number, number2);
+        break;
+      case "subtract":
+      case "-":
+        subtract(number, number2);
+        break;
+
+    }
+
+
+
+   
+  }
+
+  function divide(num1, num2) {
+    window.alert(num1 / num2);
+  }
+
+  function multiply(num1, num2) {
+    window.alert(num1 * num2);
+  }
+
+  function add(num1, num2) {
+    var answer = parseInt(num1) + parseInt(num2);
+    window.alert("The answer is " + answer);
+  }
+
+  function subtract(num1, num2) {
+    window.alert(num1 - num2);
+  }
 }
